cce19f3a07933c9a6a288f9c8280c15c2cca51ff
Kai <Kai@EEIT8309-PC>
2016-02-05 15:19:21 +0800

    First commit

---
+++ Team05WebProj/src/controller/Servlet09.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EEIT09
+ */
+@WebServlet("/EEIT09")
+public class Servlet09 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("TODAY IS RAINY DAY");
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
