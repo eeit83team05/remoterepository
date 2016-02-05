6b5bb562c7f3bc7814a6b80e46e494645cab8611
eeit8326李佩柔 <eeit8326@EEIT-PC>
2016-02-05 16:14:24 +0800

    fix

--- Team05WebProj/src/controller/Servlet29.java
+++ Team05WebProj/src/controller/Servlet29.java
@@ -16,11 +16,8 @@ public class Servlet29 extends HttpServlet {
 		System.out.println("this is test");
 		System.out.println("馬");
 		System.out.println("Hello!");
-		System.out.println("馬");
-		System.out.println("馬");
-		System.out.println("馬");
-		System.out.println("馬");		
 		System.out.println("eeit8326!!!");
+		
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		this.doGet(request, response);
