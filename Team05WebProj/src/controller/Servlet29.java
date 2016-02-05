b3f08be1bad96fffa7a4bd193f9f6a3a69b07aa1
eeit8310 <xxx@xxx.com>
2016-02-05 16:00:20 +0800

    123

--- Team05WebProj/src/controller/Servlet29.java
+++ Team05WebProj/src/controller/Servlet29.java
@@ -17,6 +17,7 @@ public class Servlet29 extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		System.out.println("this is test");
 		System.out.println("Hello!");
 	}
 
