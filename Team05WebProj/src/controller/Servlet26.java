48084c13a1edf3e593beae3d89ba50c2bf4e5ece
eeit83team05 <Student@EEIT-PC>
2016-02-05 14:09:23 +0800

    add controller.Servlet26.java

---
+++ Team05WebProj/src/controller/Servlet26.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet26
+ */
+//@WebServlet(urlPatterns={"/test.controller"})
+public class Servlet26 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("haha");
+		request.getRequestDispatcher(
+				"/test.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
